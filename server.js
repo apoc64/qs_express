5627eee57bcdca71e3db8abe5eb6dffbc7e06518
apoc64 <sschwedt@gmail.com>
2018-08-17 08:25:00 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'QS Express';
+
+app.get('/', (request, response) => {
+  response.send("Here's a response");
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
+
+module.exports = app;
