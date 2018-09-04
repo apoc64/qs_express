c4accda8c92312fb630b02a89c8648a4dcf26f0f
apoc64 <sschwedt@gmail.com>
2018-09-04 12:28:08 -0600

    Queries database for meal names with associated foods

--- model/favorites.js
+++ model/favorites.js
@@ -3,24 +3,38 @@ const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
 
 exports.getFavorites = () => {
-  return database.raw("SELECT foods.name, foods.calories, COUNT(meal_foods.food_id) AS food_count FROM meal_foods INNER JOIN foods on foods.id=meal_foods.food_id GROUP BY foods.id ORDER BY food_count DESC")
+  return database.raw("SELECT foods.id, foods.name, foods.calories, COUNT(meal_foods.food_id) AS food_count FROM meal_foods INNER JOIN foods ON foods.id=meal_foods.food_id GROUP BY foods.id ORDER BY food_count DESC")
   .then((favorites) => {
-    var rows = favorites.rows
-    const maxCount = rows[0].food_count
-    const mostEatenWithCount = rows.filter(row => row.food_count === maxCount)
-    const mostEaten = mostEatenWithCount.map(row => {
-      const obj = {
-        "name": row.name,
-        "calories": row.calories
-      }
-      return obj
-    })
-    var results = {
-      "timesEaten": parseInt(maxCount),
-      "foods": mostEaten
-    }
+    console.log(favorites.rows);
+    return database('meals')
+    .select('meals.name', 'meal_foods.food_id')
+    .innerJoin('meal_foods', 'meals.id', 'meal_foods.meal_id')
+    .then((mealFoods) => {
 
-    return results
+      console.log("Inside then");
+      console.log(mealFoods);
+      var rows = favorites.rows
+      const maxCount = rows[0].food_count
+      const mostEatenWithCount = rows.filter(row => row.food_count === maxCount)
+
+
+      const mostEaten = mostEatenWithCount.map(row => {
+        const obj = {
+          "name": row.name,
+          "calories": row.calories,
+          "mealsWhenEaten": "hello"
+        }
+        return obj
+      })
+
+      var results = {
+        "timesEaten": parseInt(maxCount),
+        "foods": mostEaten
+      }
+
+      return results
+    })
+
 
   });
 };
