4e21f820c6f0a306e36af7c8562bc091a0035f68
apoc64 <sschwedt@gmail.com>
2018-09-04 13:47:22 -0600

    Creates Yummly URL

--- model/recipes.js
+++ model/recipes.js
@@ -2,14 +2,20 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
 
-const yummlyKey = process.env.YUMMLY_KEY
+const yummlyID = process.env.YUMMLY_KEY
+const yummlyKey = process.env.YUMMLY_ID
 const foodModel = require('./foods.js')
 
 
 exports.getRecipes = (id) => {
   return foodModel.getFood(id)
   .then((food) => {
-    console.log(food);
-    console.log(yummlyKey);
+    const url = yummlyURL(food.name)
+    console.log(url);
+
   })
+}
+
+function yummlyURL(name) {
+  return `https://api.yummly.com/v1/api/recipes?_app_id=${yummlyID}&_app_key=${yummlyKey}&q=${name}`
 }
